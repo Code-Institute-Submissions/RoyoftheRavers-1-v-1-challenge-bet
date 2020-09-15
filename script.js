f13b44d658539dab895138f9857f80cbf4f17683
john00neill <john2984@live.com>
2020-09-15 21:23:44 +0000

    New Modal added

--- script.js
+++ script.js
@@ -5,28 +5,3 @@ $( function() {
     });
   } );
 
-  // Get the modal
-var modal1 = document.getElementById("modal1");
-
-// Get the button that opens the modal
-var btn1 = document.getElementById("start1");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn1.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
