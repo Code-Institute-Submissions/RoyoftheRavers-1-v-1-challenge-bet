19573382ebde8216fc9e70d9f5004594891c2140
john00neill <john2984@live.com>
2020-09-17 17:34:42 +0000

    First modal updated for match 1

--- script.js
+++ script.js
@@ -13,16 +13,22 @@ let gameStart = document.getElementById("start1");
 gameStart.addEventListener("click", function (){
   
   // Get the user's first name
-  let firstName = document.getElementById("first-name");
+  let firstName = document.getElementById("first-name").value;
+  console.log(firstName);
+  let updateModal = document.getElementById("mb1").innerHTML = `${firstName}, good news, your bet is on!<br> 
+  You have chosen`;
+  console.log(updateModal);
   // Get the user's last name
-  let lastName = document.getElementById("last-name");
+  /* let lastName = document.getElementById("last-name").value;
   // Get the Amount chosen
   let gameAmount = document.getElementById("amount").value;
   // Get the Prediction chosen
   let prediction1 = document.getElementById("prediction").value;
 
+  
+
     
-   let updateModal = document.getElementById("mb1").innerHTML = ` ${firstName}, you have chosen${prediction1} and are have bet €${gameAmount}. Best of luck!`;
+   let updateModal = document.getElementById("mb1").innerHTML = ` ${firstName}, you have chosen${prediction1} and have bet €${gameAmount}. Best of luck!`;
    console.log(updateModal); 
    
    /*var messageTwo = 'Hi ${first-Name}, your team choice is ${prediction1} and your bet is ${gameAmount}';
@@ -31,15 +37,18 @@ gameStart.addEventListener("click", function (){
 
     // Game functions
 
-    function runGameManUtdLiv(firstName, gameAmount, prediction) {
+    /*function runGameManUtdLiv(firstName, gameAmount, prediction) {
         document.getElementById("mb1").innerHTML = "<h3>Good news " + name + ", your friend has accepted your challenge, game on!<br>;" +
         "</h3><h3>The amount of your bet is €"+ gameAmount + ".<br> The team you chose to win is " + prediction + "." + "</h3>";
         console.log("Match 1..");
-    }
+    runGameManUtdLiv();*/
+    
 
-
+    
 
 
   // For final modal define variables
 
   // Use method chaining to update classes based on button click and if statement
+
+  localStorage.setItem("match1result", "Man United");
