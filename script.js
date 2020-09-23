2bde259905e76e3654173e5add0e2d08d5eca92a
john00neill <john2984@live.com>
2020-09-23 11:05:55 +0000

    If statement to validate empty input modal 1 updated

--- script.js
+++ script.js
@@ -27,22 +27,25 @@ localStorage.setItem("match4result", "The Field");
 localStorage.setItem("match5result", "Dublin");
 
 // Match 1: Man United v Liverpool
+
+// If statement to validate empty fields
+    if(document.getElementById("first-name1").value === "") {
+        alert('Please enter your first name');
+        return false;
+
+    }  else if(document.getElementById("last-name").value === "") {
+        alert('Please enter your last name');
+        return false;
+
+    }   else { 
+
 // The game starts with this button for 'Challenge Friend'
 let gameStart = document.getElementById("start1a");
 
 // When the user clicks the button, the script gathers the choices
 gameStart.addEventListener("click", function (){
 
-    // If statement to validate empty fields
-    if(document.getElementById("first-name1").value === "") {
-        alert('Please enter your first name');
-        return false;
-        
-
-    }  else if(document.getElementById("last-name").value === "") {
-        alert('Please enter your last name');
-        return false;
-    }   else { 
+    
 
     // Get the user's first name
     let firstName1 = document.getElementById("first-name1").value;
@@ -65,7 +68,8 @@ gameStart.addEventListener("click", function (){
     PS when the match is over you can check the result below:`;
     console.log(updateModal1);
     }
-});
+)};
+    
 
 // The result generating starts with clicking the 'See Result' button
 let seeResult1 = document.getElementById("start1b");
